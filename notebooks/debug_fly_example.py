665d07e99088317fe25a998cc8281a0928f8d543
Kristin Branson <bransonk@janelia.hhmi.org>
2024-06-24 15:03:50 -0400

    Updated the debug_fly_examples notebook to actually be formatted correctly :) Updated environments to run notebooks

--- notebooks/debug_fly_example.py
+++ notebooks/debug_fly_example.py
@@ -1,28 +1,27 @@
 # ---
 # jupyter:
 #   jupytext:
+#     custom_cell_magics: kql
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.2
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
-# ## Imports
-# +
-
+# %%
+## Imports
 import numpy as np
 import torch
 import matplotlib
-matplotlib.use('TkAgg')
+#matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import os
 import pickle
-
 from flyllm.io import read_config
 from flyllm.utils import get_dct_matrix, compute_npad
 from flyllm.config import featglobal, featrelative
@@ -33,19 +32,14 @@ from flyllm.pose import PoseLabels, FlyExample, ObservationInputs
 from flyllm.features import kp2feat
 from flyllm.plotting import debug_plot_pose, debug_plot_sample, debug_plot_batch_traj
 
-# -
-
-# ## Set parameters, read in data, config
-# +
-
+# %%
+## Set parameters, read in data, config
 tmpsavefile = '/groups/branson/home/bransonk/behavioranalysis/code/MABe2022/tmp_small_usertrainval.pkl'
 configfile = '/groups/branson/home/bransonk/behavioranalysis/code/MABe2022/config_fly_llm_multitime_20230825.json'
-
 # configuration parameters for this model
 config = read_config(configfile)
 
 # override parameters in config file for testing
-
 # debug velocity representation
 config['compute_pose_vel'] = True
 # debug dct
@@ -76,7 +70,7 @@ if config['dct_tau'] is not None and config['dct_tau'] > 0:
 else:
   dct_m = None
   idct_m = None
-
+  
 # how much to pad outputs by -- depends on how many frames into the future we will predict
 npad = compute_npad(config['tspred_global'],dct_m)
 chunk_data_params = {'npad': npad}
@@ -89,10 +83,10 @@ compute_feature_params = {
     "compute_pose_vel": config['compute_pose_vel'],
     "discreteidx": config['discreteidx'],
 }
-
 # function for computing features
 reparamfun = lambda x,id,flynum,**kwargs: compute_features(x,id,flynum,scale_perfly,outtype=np.float32,
                                                           **compute_feature_params,**kwargs)
+
 # chunk the data if we didn't load the pre-chunked cache file
 print('Chunking training data...')
 X = chunk_data(data,config['contextl'],reparamfun,**chunk_data_params)
@@ -120,7 +114,6 @@ dataset_params = {
 train_dataset_params = {
   'input_noise_sigma': config['input_noise_sigma'],
 }
-
 compute_feature_params = {
     "simplify_out": config['simplify_out'],
     "simplify_in": config['simplify_in'],
@@ -133,14 +126,11 @@ compute_feature_params = {
 print('Creating training data set...')
 train_dataset = FlyMLMDataset(X,**train_dataset_params,**dataset_params)
 
-# -
-
-# ## compare flyexample initialized from FlyMLMDataset and from keypoints directly
-# +
+# %%
+## compare flyexample initialized from FlyMLMDataset and from keypoints directly
 
 # flyexample from training dataset
 flyexample = train_dataset.data[0]
-
 contextlpad = train_dataset.contextl + npad + 1
 Xkp = data['X'][:,:,flyexample.metadata['t0']:flyexample.metadata['t0']+contextlpad,:]
 flyexample_kp = FlyExample(Xkp=Xkp,scale=scale_perfly[:,flyexample.metadata['id']],
@@ -162,20 +152,15 @@ err = np.max(np.abs(multi-multi_kp))
 print('max diff between multi labels: %e'%err)
 assert err < 1e-9
 
-# -
-
-# ## compare pose feature representation from original chunked data and PoseLabels.multi
-# +
-
+# %%
+## compare pose feature representation from original chunked data and PoseLabels.multi
 err_chunk_multi = np.max(np.abs(X[0]['labels']-flyexample.labels.get_multi(use_todiscretize=True,zscored=False)))
 print('max diff between chunked labels and multi: %e'%err_chunk_multi)  
 assert err_chunk_multi < 1e-3
 
-# -
 
-# ## compare next frame representation
-# +
-
+# %%
+## helper functions
 def data_to_kp_from_metadata(data,metadata,ntimepoints):
   t0 = metadata['t0']
   flynum = metadata['flynum']
@@ -212,12 +197,15 @@ def compare_dicts(old_ex,new_ex,maxerr=None):
 
   return
 
+
+# %%
+## compare next frame representation
+
 # extract frames associated with metadata in flyexample
 contextl = flyexample.ntimepoints
 datakp,id = data_to_kp_from_metadata(data,flyexample.metadata,contextl)
 # compute next frame pose feature representation directly
 datafeat = kp2feat(datakp.transpose(1,2,0),scale_perfly[:,id])[...,0].T
-
 if config['compute_pose_vel']:
 
   # compare next frame rep computed from FlyMLMDataset to those from flyexample
@@ -227,7 +215,6 @@ if config['compute_pose_vel']:
   err_chunk_next = np.max(np.abs(chunknext-examplenext))
   print('max diff between chunked labels and next: %e'%err_chunk_next)
   assert err_chunk_next < 1e-3
-
 else:
   
   # compare next frame rep computed from FlyMLMDataset to those from flyexample
@@ -263,7 +250,7 @@ else:
   err_example_data_feat = np.max(np.abs(datafeat[:,featrelative]-examplefeat[:,featrelative]))
   print('max diff between data and example relative features: %e'%err_example_data_feat)
   assert err_example_data_feat < 1e-3
-
+  
 # compare next frame keypoints
 examplekp = flyexample.labels.get_next_keypoints(use_todiscretize=True)
 err_mean_example_data_kp = np.mean(np.abs(datakp[:]-examplekp))
@@ -271,16 +258,14 @@ print('mean diff between data and example keypoints: %e'%err_mean_example_data_k
 err_max_example_data_kp = np.max(np.abs(datakp[:]-examplekp))
 print('max diff between data and example keypoints: %e'%err_max_example_data_kp)
 
-debug_plot_pose(flyexample,data=data)  
+# plot
+_ = debug_plot_pose(flyexample,data=data)  
 # elements of the list tspred_global that are smaller than contextl
 tsplot = [t for t in train_dataset.tspred_global if t < contextl]
-debug_plot_pose(flyexample,pred=flyexample,tsplot=tsplot)
+_ = debug_plot_pose(flyexample,pred=flyexample,tsplot=tsplot)
 
-# -
-
-# ## compare to legacy fly_llm code, which only supported velocities
-# +
-
+# %%
+## compare to legacy fly_llm code, which only supported velocities
 if config['compute_pose_vel']:
 
   import flyllm.legacy.old_fly_llm as old_fly_llm
@@ -312,28 +297,22 @@ if config['compute_pose_vel']:
   max_err_init = torch.max(torch.abs(new_ex['init']-old_ex['init'])).item()
   print('max error between old and new init: %e'%max_err_init)
   assert max_err_init < 1e-3
-  
-# -
 
-# ## check global future predictions
-# +
-
+# %%
+## check global future predictions
 print('\nChecking that representations of many frames into the future match')
+flynum = flyexample.metadata['flynum']
 for tpred in flyexample.labels.tspred_global:
   examplefuture = flyexample.labels.get_future_globalpos(use_todiscretize=True,tspred=tpred,zscored=False)
   t = flyexample.metadata['t0']+tpred
-  flynum = flyexample.metadata['flynum']
   datakpfuture = data['X'][:,:,t:t+contextl,flynum]
   datafeatfuture = kp2feat(datakpfuture,scale_perfly[:,id])[...,0].T  
   err_global_future = np.max(np.abs(datafeatfuture[:,featglobal]-examplefuture[:,0,:]))
   print(f'max diff between data and t+{tpred} global prediction: {err_global_future:e}')
   assert err_global_future < 1e-3
 
-# -
-
-# ## check relative future predictions
-# +
-
+# %%
+## check relative future predictions
 if flyexample.labels.ntspred_relative > 1:
   examplefuture = flyexample.labels.get_future_relative_pose(zscored=False,use_todiscretize=True)
   for tpred in range(1,flyexample.labels.ntspred_relative):
@@ -343,12 +322,10 @@ if flyexample.labels.ntspred_relative > 1:
     err_relative_future = np.max(np.abs(datafeatfuture[:,featrelative]-examplefuture[:,tpred-1,:]))
     print(f'max diff between data and t+{tpred} relative prediction: {err_relative_future:e}')
     assert err_relative_future < 1e-3
-    
-# -
 
-# ## check get_train_example and constructor from train_example
-# +
-    
+# %%
+## check get_train_example and constructor from train_example
+
 # get a training example
 print('\nComparing training example from dataset to creating a new FlyExample from that training example, and converting back to a training example')
 trainexample = train_dataset[0]
@@ -356,10 +333,8 @@ flyexample1 = FlyExample(example_in=trainexample,dataset=train_dataset)
 trainexample1 = flyexample1.get_train_example()
 compare_dicts(trainexample,trainexample1,maxerr=1e-9)
 
-# -
-
-# ## check constructor from train_example
-# +
+# %%
+## check constructor from batch
 
 # initialize example from batch
 print('\nComparing training batch to FlyExample created from that batch converted back to a training batch')
@@ -373,15 +348,23 @@ example_batch = FlyExample(example_in=raw_batch,dataset=train_dataset)
 trainbatch1 = example_batch.get_train_example()
 compare_dicts(raw_batch,trainbatch1,maxerr=1e-9)
 
-debug_plot_pose(example_batch,data=data)
-debug_plot_sample(example_batch,train_dataset)
-debug_plot_batch_traj(example_batch,train_dataset,nsamplesplot=1)
+_ = debug_plot_pose(example_batch,data=data)
+_ = debug_plot_sample(example_batch,train_dataset)
+_ = debug_plot_batch_traj(example_batch,train_dataset,nsamplesplot=1)
 #old_fly_llm.debug_plot_sample_inputs(old_train_dataset,raw_batch)
 
-# -
+# %%
+## check constructor from keypoints
+flyexample = train_dataset.data[0]
+Xkp0 = data['X'][:,:,flyexample.metadata['t0']:flyexample.metadata['t0']+contextlpad,:]
+Xkp = flyexample.labels.get_next_keypoints()
+train_example0 = flyexample.get_train_example()
+flyexample_kp = FlyExample(Xkp=Xkp,scale=scale_perfly[:,flyexample.metadata['id']],
+                            flynum=flyexample.metadata['flynum'],metadata=flyexample.metadata,
+                            **flyexample.get_params())
+train_example_kp = flyexample_kp.get_train_example()
 
-# ## done
-# +
-
+# %%
+## done
 print('Goodbye!')
 plt.show(block=True)
