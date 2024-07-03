dbaa31f41ed981043df81b626199e96130d0ecb5
eyjolfsdottire <eyjolfsdottire@bransonk-ws11.hhmi.org>
2024-07-03 11:29:53 -0400

    Cleanup notebook

--- notebooks/debug_fly_llm.py
+++ notebooks/debug_fly_llm.py
@@ -77,64 +77,6 @@ config['augment_flip'] = False
 
 config['intrainfile']
 
-
-
-os.path.relpath(test_train_path, test_dir)
-
-# +
-from flyllm.io import json_load_helper
-
-config_file = "/groups/branson/home/eyjolfsdottire/code/MABe2022/config_fly_llm_multitimeglob_discrete_20230907.json"
-config = json_load_helper(config_file)
-config
-
-# +
-# Generate data for testing
-import json
-from apf.data import load_raw_npz_data, debug_less_data
-
-config_file = "/groups/branson/home/eyjolfsdottire/code/MABe2022/config_fly_llm_multitimeglob_discrete_20230907.json"
-config = json_load_helper(config_file)
-
-test_dir = '/groups/branson/bransonlab/test_data_apf/'
-
-# Create smaller train and validation data and save it to the test directory
-train_path = os.path.join(config['datadir'], config['intrainfilestr'])
-train_data = load_raw_npz_data(train_path)
-debug_less_data(train_data, n_frames_per_video=2000, max_n_videos=5)
-test_train_path = os.path.join(pardir, 'small_intrainfile.npz')
-np.savez(test_train_path, **train_data)
-
-val_path = os.path.join(config['datadir'], config['invalfilestr'])
-val_data = load_raw_npz_data(val_path)
-debug_less_data(val_data, n_frames_per_video=2000, max_n_videos=5)
-test_val_path = os.path.join(pardir, 'small_invalfile.npz')
-np.savez(test_val_path, **val_data)
-
-config['datadir'] = test_dir
-config['savedir'] = os.path.join(test_dir, 'llmnets')
-config['intrainfilestr'] = os.path.relpath(test_train_path, test_dir)
-config['invalfilestr'] = os.path.relpath(test_val_path, test_dir)
-config['batch_size'] = 8
-config['num_train_epochs'] = 2
-config['niterplot'] = 2
-config['n_layers'] = 3
-
-with open(os.path.join(test_dir, 'test_config.json'), 'w') as f:
-    json.dump(config, f)
-
-# +
-import matplotlib
-matplotlib.use('tkagg')
-
-from flyllm.run_flyllm import main
-
-config_file = "/groups/branson/bransonlab/test_data_apf/test_config.json"
-main(config_file)
-# -
-
-
-
 # load raw data
 data, scale_perfly = load_and_filter_data(config['intrainfile'], config)
 valdata, val_scale_perfly = load_and_filter_data(config['invalfile'], config)
