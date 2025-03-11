96096e19aaa1c28999a7a14cdbe16dc22d185a58
eyjolfsdottire <eyjolfsdottire@bransonk-ws11.hhmi.org>
2025-03-11 05:42:17 -0400

    Use available variable instead of compute

--- apf/models.py
+++ apf/models.py
@@ -1049,7 +1049,7 @@ def initialize_model(config, train_dataset, device):
 
         if train_dataset.discretize:
             # Before refactor this was: config['weight_discrete'] = len(config['discreteidx']) / nfeatures
-            config['weight_discrete'] = len(train_dataset.discreteidx) / train_dataset.d_output
+            config['weight_discrete'] = train_dataset.d_output_discrete / train_dataset.d_output
             if config['modeltype'] == 'mlm':
                 criterion = mixed_masked_criterion
             else:
