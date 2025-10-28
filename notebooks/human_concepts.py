b54ed6dc0493a064a04dfa0dbfd7144ac0e2cc29
Kristin Branson <bransonk@janelia.hhmi.org>
2025-10-28 07:36:40 -0400

    Debugged convolution padding issues.

--- notebooks/human_concepts.py
+++ notebooks/human_concepts.py
@@ -29,6 +29,8 @@ from flyllm.features import featglobal, get_sensory_feature_idx
 import time
 from flyllm.plotting import plot_fly, plot_flies
 
+thresh_stopped = 5
+thresh_walking = 15
 
 
 # %%
@@ -63,7 +65,7 @@ print(rawdata['isstart'].shape)
 flyidx = 1
 frame = 0
 fig,ax = plt.subplots()
-ax.plot(rawdata['X'][:,0,frame,flyidx], rawdata['X'][:,1,frame,flyidx], 'k.')
+ax[0].plot(rawdata['X'][:,0,frame,flyidx], rawdata['X'][:,1,frame,flyidx], 'k.')
 plot_fly(rawdata['X'][:,:,frame,flyidx], ax=ax, kpt_marker='o', kpt_ms=20, textlabels='keypoints')
 ax.axis('equal')
 
@@ -78,12 +80,20 @@ ax.axis('equal')
 # %%
 centeridx = 7
 fps = 150.
+sigma = 2 # in frames
 
 # compute the velocity vector
 # X is # (nkpts, 2, nframes, nflies)
 vel = rawdata['X'][centeridx,:,1:,:] - rawdata['X'][centeridx,:,:-1,:]  # (2, nframes-1, nflies)
 vel[:,rawdata['isstart'][1:,:]] = np.nan  # set velocity between end frames and start frames to nan
-vel_mmps = vel * fps  # convert to mm/s
+# make a gaussian filter to smooth vel
+from scipy.ndimage import gaussian_filter1d
+if sigma > 0:
+    vel_fil = gaussian_filter1d(vel, sigma=sigma, axis=1)
+else:
+    vel_fil = vel
+
+vel_mmps = vel_fil * fps  # convert to mm/s
 
 velmag = np.linalg.norm(vel, axis=0)  # (nframes-1, nflies)
 velmag_mmps = velmag * fps  # convert to mm/s
@@ -98,7 +108,10 @@ ax[0].set_ylim([-maxvelmag, maxvelmag])
 ax[1].plot(vel_mmps[1,:,flyidx], '-')
 ax[1].set_ylabel('Vy')
 ax[1].set_ylim([-maxvelmag, maxvelmag])
-ax[2].plot(velmag_mmps[:,flyidx], '-')
+ax[2].plot(velmag_mmps[:,flyidx], '-', label='|V|')
+ax[2].axhline(thresh_stopped, color='r', linestyle='--', label='stopped thresh')
+ax[2].axhline(thresh_walking, color='g', linestyle='--', label='walking thresh')
+ax[2].legend()
 ax[2].set_ylabel('|V|')
 ax[2].set_ylim([0, maxvelmag])
 ax[2].set_xlabel('Frame')
@@ -112,9 +125,97 @@ ax[2].set_xlim(0,10000)
 minvelmag = 1e-1
 maxvelmag = 100
 nbins = 100
+
 bin_edges = np.logspace(np.log10(minvelmag), np.log10(maxvelmag), num=nbins+1)
 counts,bin_edges = np.histogram(velmag_mmps[~np.isnan(velmag_mmps)],bins=bin_edges)
 fig,ax = plt.subplots()
 ax.bar(bin_edges[:-1], counts, width=np.diff(bin_edges), align='edge')
 ax.set_xlabel('Velocity magnitude (mm/s)')
 ax.set_ylabel('Fly-frames')
+ylim = ax.get_ylim()
+ax.axvline(thresh_stopped, color='r', linestyle='--', label='stopped')
+ax.axvline(thresh_walking, color='g', linestyle='--', label='walking')
+ax.legend()
+
+# %%
+# find time points when the fly is stopped for at least tstopped seconds and transitions to walking
+# velmag is # (nframes-1, nflies)
+tstopped = .5 # seconds
+tfuture = 1 # seconds
+tstopped_frames = int(tstopped * fps)
+tfuture_frames = int(tfuture * fps)
+filstopped = np.ones((tstopped_frames,1),dtype=int)
+filfuture = np.ones((tfuture_frames,1),dtype=int)
+nflies = velmag_mmps.shape[1]
+# nflies = 1
+
+from scipy.ndimage import convolve
+
+def same2valid(x,filsize,debug=False):
+    padright = filsize // 2
+    padleft = filsize - padright - 1
+    if debug:
+        print(f'padleft = {padleft}, padright = {padright}, x[padleft-1] = {x[padleft-1]}, x[padleft] = {x[padleft]}, x[-padright-1] = {x[-padright-1]}, x[-padright] = {x[-padright]}')
+        assert padleft == 0 or np.all(np.isnan(x[:padleft]))
+        assert padright == 0 or np.all(np.isnan(x[-padright:]))
+        assert not np.any(np.isnan(x[padleft:-padright]))
+    return x[padleft:-padright]
+
+# isstop = np.zeros((1000,nflies))
+# isstop[:500] = 1
+isstop = velmag_mmps <= thresh_stopped
+wasstopped = convolve(isstop.astype(int), filstopped, mode='constant', cval=0) >= tstopped_frames
+wasstopped = same2valid(wasstopped, tstopped_frames)
+wasstopped = np.r_[np.zeros((tstopped_frames-1, nflies), dtype=bool),wasstopped]
+
+# iswalk = np.zeros((1000,nflies))
+# iswalk[500-1+tfuture_frames:] = 1
+iswalk = velmag_mmps >= thresh_walking
+willwalk = convolve(iswalk.astype(int), filfuture, mode='constant', cval=0) >= 1
+willwalk = same2valid(willwalk, tfuture_frames)
+willwalk = np.r_[willwalk[1:],np.zeros((tfuture_frames, nflies), dtype=bool)]
+
+willstop = convolve(isstop.astype(int), filfuture, mode='constant', cval=0) >= tfuture_frames
+willstop = same2valid(willstop, tfuture_frames)
+willstop = np.r_[willstop[1:],np.zeros((tfuture_frames, nflies), dtype=bool)]
+
+startsmoving = wasstopped & willwalk
+staysstopped = wasstopped & willstop
+
+# tstartsmoving = np.nonzero(startsmoving)[0]
+# print(f'len(tstartsmoving) = {len(tstartsmoving)}')
+# for t in tstartsmoving:
+#     print(f't = {t}')
+#     for off in [-1,0,1]:
+#         print(f't = {t+off}, isstop[{t+off}] = {isstop[t+off,0]}, iswalk[{t+off}] = {iswalk[t+off]}, all(isstop[{t+off-tstopped_frames+1}:{t+off+1}]) = {np.all(isstop[t+off-tstopped_frames+1:t+off+1])}, any(iswalk[{t+off+1}:{t+off+tfuture_frames+1}]) = {np.any(iswalk[t+off+1:t+off+tfuture_frames+1,0])}')
+
+# %%
+# plot results
+
+fig,ax = plt.subplots(2,1, figsize=(30,10),sharex=True)
+tplot = 50000
+
+ax[0].plot(velmag_mmps[:tplot,flyidx], '-', label='|V|')
+ax[0].plot(np.nonzero(startsmoving[:tplot,flyidx])[0], velmag_mmps[:tplot][startsmoving[:tplot,flyidx], flyidx], 'g.', label='starts moving')
+ax[0].plot(np.nonzero(staysstopped[:tplot,flyidx])[0], velmag_mmps[:tplot][staysstopped[:tplot,flyidx], flyidx], 'r.', label='stays stopped')
+ax[0].axhline(thresh_stopped, color='r', linestyle='--', label='stopped thresh')
+ax[0].axhline(thresh_walking, color='g', linestyle='--', label='walking thresh')
+ax[0].legend()
+ax[0].set_ylabel('|V|')
+ax[0].set_ylim([0, maxvelmag])
+
+ax[1].plot(isstop[:tplot,flyidx],'-',label='isstop')
+ax[1].plot(1.1+iswalk[:tplot,flyidx].astype(float),'-',label='iswalk')
+ax[1].plot(2.2+wasstopped[:tplot,flyidx].astype(float),'-',label='wasstopped')
+ax[1].plot(3.3+willwalk[:tplot,flyidx].astype(float),'-',label='willwalk')
+ax[1].plot(4.4+willstop[:tplot,flyidx].astype(float),'-',label='willstop')
+ax[1].plot(5.5+startsmoving[:tplot,flyidx].astype(float),'-',label='startsmoving')
+ax[1].plot(6.6+staysstopped[:tplot,flyidx].astype(float),'-',label='staysstopped')
+ax[1].set_yticks(np.arange(7)*1.1+.5)
+ax[1].set_yticklabels(['isstop','iswalk','wasstopped','willwalk','willstop','startsmoving','staysstopped'])
+
+
+ax[1].set_xlabel('Frame')
+#ax[1].set_xlim(14000,16000)
+
+
